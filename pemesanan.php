adb21289c24f66aa38ebee2d80200c66ae5bcb99
afdifznsyh <afdillah2003@gmail.com>
2023-05-06 20:32:45 +0800

    membuatuser

---
+++ pemesanan.php
@@ -0,0 +1,36 @@
+<?php include 'layout/header.php' ?>
+
+<div class="border border-1 shadow shadow-lg rounded" style=" padding: 30px; padding-bottom: 300px;">
+    <table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama Produk</th>
+            <th scope="col">Jumlah</th>
+            <th scope="col">Total Harga</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php 
+            $query = "SELECT * FROM pemesanan INNER JOIN produk ON produk.id = pemesanan.id_produk";
+            $result = mysqli_query($conn, $query);
+            $i=1;
+            while($row = mysqli_fetch_assoc($result)) : 
+        ?>
+
+        <tr>
+            <td><?= $i ?></td>
+            <td><?= $row['nama'] ?></td>
+            <td><?= $row['jumlah'] ?></td>
+            <td><?= $row['total_harga'] ?></td>
+        </tr>
+
+        <?php 
+            $i++;
+            endwhile;
+        ?>
+    </tbody>
+    </table>
+</div>
+
+<?php include 'layout/footer.php' ?>
