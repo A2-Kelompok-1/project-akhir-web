3e9ff4725b92cca134db37320007d137f9c7cb07
erniveronica <erniveronicasidabutar@gmail.com>
2023-05-12 11:52:42 +0800

    memperbaiki codingan admin

--- admin/delete.php
+++ admin/delete.php
@@ -1,31 +1,38 @@
 <?php
 
-// memanggil file konfigurasi koneksi ke database
+//koneksi ke database
 require '../koneksi.php';
 
-// mengambil nilai id --> (mencari data produk)
+//identifier menentukan data produk 
 $id = $_GET['id'];
 
-// mengecek paramater id dan melakukan eksekusi
 if($id){
-    // mencari data produk
     $query = "SELECT gambar FROM produk WHERE id = '$id'";
     $result = mysqli_query($conn, $query);
+    //apakah mengembalikan tepat 1 baris hasil atau tidak
     if(mysqli_num_rows($result) == 1){
+        //mengambil 1 baris hasil query
         $row = mysqli_fetch_assoc($result);
         $folder = "../image/".$row['gambar'];
-        // menghapus file gambar yang terkait dengan data produk yang dihapus
         unlink($folder);
     }
-    // menghapus data produk
+    //menghapus data
     $_query = "DELETE FROM produk WHERE id = $id";
-    // menjalankan query yang dibuat 
     mysqli_query($conn, $_query);
-    
-    echo "<script>
-        alert('Berhasil Menghapus Data');
-        document.location.href = 'view.php';
-    </script>";
-}
 
+    //menampilkan pesan 
+    echo "<body><script src='https://cdn.jsdelivr.net/npm/sweetalert2@11.0.10'></script>
+    <script>
+    Swal.fire({
+        icon: 'success',
+        title: 'Berhasil Menghapus Data',
+        showConfirmButton: false,
+        timer: 1500
+    }).then(function() {
+        window.location.href = 'view.php';});
+    </script></body>";
+}
 ?>
+
+<!-- link CSS -->
+<link rel="stylesheet" href="../style.css">
--- admin/view.php
+++ admin/view.php
@@ -1,41 +1,58 @@
 <?php
 
-include "head.php";
+//memasukan file kedalam halaman html
+include 'head.php';
 
-require "../koneksi.php";
+//koneksi ke database
+require '../koneksi.php';
 
 $query = "SELECT * FROM produk";
 $result = mysqli_query($conn, $query);
-
 ?>
 
 <section>
     <div class="container">
         <p class="h2 p-2 text-center">Data Produk</p>
         <a id="tambah" href="insert.php" class="btn btn-outline-dark"><i class="bi bi-plus-circle"></i> Tambah Produk</a>
-		<table class="table table-bordered">
-			<thead class="table-dark">
+		<table id="example" class="display" style="width:100%">
+			<thead>
 				<tr>
-					<th scope="col">No</th>
-					<th scope="col">Nama Produk</th>
-                    <th scope="col">Gambar Produk</th>
-                    <th scope="col">Deskripsi</th>
-					<th scope="col">Harga</th>
-					<th scope="col" colspan="2">Action</th>
+					<th>No</th>
+					<th>Nama Produk</th>
+                    <th>Gambar Produk</th>
+                    <th>Deskripsi</th>
+					<th>Harga</th>
+					<th>Edit</th>
+                	<th>Hapus</th>
 				</tr>
 			</thead>
 
 		    <?php
+			//loop --> mengambil setiap baris data yang ditemukan di query
             $i=1;
             while($row = mysqli_fetch_assoc($result)) { ?>
 			<tr>
-				<td style="text-align: center;"><?php echo $i ?></td>
+				<td><?php echo $i ?></td>
 				<td><?php echo $row['nama']?></td>
 				<td><img src="../image/<?php echo $row['gambar'] ?>" width="150px"></td>
 				<td><?php echo $row['deskripsi']?></td>
-				<td style="text-align: center;"><?php echo $row['harga']?></td>
-				<td><a href="update.php?id=<?php echo $row['id'] ?>"><i class="bi bi-pencil-square"></i></a></td>
-				<td><a onclick="return confirm('Anda Yakin menghapus data produk?')" href="delete.php?id=<?php echo $row['id'] ?>"><i class="bi bi-trash-fill"></i></a></td>
+				<td><?php echo $row['harga']?></td>
+				<td><a href="update.php?id=<?php echo $row['id'] ?>"><i class="bi bi-pencil-square" id="ikon"></i></a></td>
+				<td><a onclick="event.preventDefault();
+					Swal.fire({
+						title: 'Anda yakin ingin menghapus data produk?',
+						icon: 'warning',
+						showCancelButton: true,
+						confirmButtonText: 'Ya',
+						cancelButtonText: 'Tidak'
+					}).then((result) => {
+						if (result.isConfirmed) {
+						window.location.href = 'delete.php?id=<?php echo $row['id'] ?>';
+						}
+					});">
+					<i class="bi bi-trash-fill"></i>
+					</a>
+				</td>
 			</tr>
             <?php $i++ ?>
 		    <?php } ?>
@@ -43,3 +60,10 @@ $result = mysqli_query($conn, $query);
     </div>
 </section>
 <footer class="border">© 2023 - IT Development <span id="text">@Glamour </span>Shop - All Rights Reserved</footer>
+
+<!-- Datatables -->
+<script>
+        $(document).ready(function () {
+            $('#example').DataTable();
+        });
+</script>
