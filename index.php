d1fa81f7e9ab636f7bcef390d5b05dc6dcf924e1
erniveronica <erniveronicasidabutar@gmail.com>
2023-05-15 07:20:47 +0800

    merapikan codingan

--- index.php
+++ index.php
@@ -16,24 +16,57 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a id="nav-user" class="nav-link" href="index.php">Beranda</a>
+                <li class="nav-item">
+                    <a id="nav-user" class="nav-link" href="#about">Tentang Kami</a>
                 </li>
                 <li class="nav-item">
-                    <a id="nav-user" class="nav-link" href="about.php">Tentang Kami</a>
+                    <a id="nav-user" class="nav-link" href="#produk">Produk</a>
                 </li>
                 <li class="nav-item">
-                    <a id="nav-user" class="nav-link" href="barang.php">Produk</a>
+                    <a id="nav-user" class="nav-link" href="#petunjuk">Petunjuk</a>
                 </li>
                 <li class="nav-item">
-                    <a id="nav-user" class="nav-link" href="pemesanan.php">Pesanan</a>
-                </li>
-                <li class="nav-item">
-                    <a id="nav-user" class="nav-link" href="login.php">Login</a>
+                    <a id="nav-user" class="nav-link" href="login.php">Pesan</a>
                 </li>
             </ul>
         </div>
     </nav>
+
+    <section>
+        <div id="about" class="awal">
+            <p class="h3 text-center">Tentang Kami</p>
+            <p class="tentang">Glamour Shop adalah sebuah toko yang menyediakan berbagai macam aksesoris berkualitas 
+            tinggi untuk meningkatkan gaya dan keindahan Anda. Dengan fokus pada tren terkini dan kebutuhan fashion, 
+            kami menawarkan beragam pilihan produk aksesoris yang elegan dan modis.</p>
+        </div>
+
+        <div id="produk" class="awal">
+            <p class="h3 text-center">Aksesoris Best Seller</p>
+            <p class="tentang text-center">Berikut beberapa aksesoris best seller di Glamour Shop</p>
+
+            <div class="gambar_produk">
+                <div class="row g-0">
+                    <div class="col-md-4"><img class="w-100" src="image/2023-04-30 010558.jpg" alt=""></div>
+                    <div class="col-md-4"><img class="w-100" src="image/2023-04-30 010732.jpg" alt=""></div>
+                    <div class="col-md-4"><img class="w-100" src="image/2023-04-30 011151.jpg" alt=""></div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-md-4"><img class="w-100" src="image/2023-04-29 235922.jpg" alt=""></div>
+                    <div class="col-md-4"><img class="w-100" src="image/2023-05-04 142811.jpg" alt=""></div>
+                    <div class="col-md-4"><img class="w-100" src="image/2023-04-30 003910.jpg" alt=""></div>
+                </div>
+            </div>    
+        </div>
+
+        <div id="petunjuk" class="awal">
+            <p class="h3 text-center">Informasi (Petunjuk)</p>
+            <p class="tentang">Untuk pembelian aksesoris, Anda harus melakukan pemesanan produk terlebih dahulu 
+            pada bagian pesan, agar tim staff dapat menyiapkan barang permintaan Anda.</p>
+        </div>
+        
+    </section>
+    <footer class="border">© 2023 - IT Development <span id="text">@Glamour </span>Shop - All Rights Reserved</footer>
+
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
